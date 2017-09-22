46e5e8ad274cbd9ae16250ac09713dd20ba078d6
Baska Batbold & Katherine Thongsakounh <pair+itsbaska.kthong3@devbootcamp.com>
2017-09-22 18:14:22 -0500

    add comments for guess logic

--- app/models/guess.rb
+++ app/models/guess.rb
@@ -10,3 +10,20 @@ class Guess < ApplicationRecord
 
   validates :counter, :completed, :round_id, :card_id, { :presence => true }
 end
+
+
+=begin
+
+a counter for guesses-- both cards and rounds.
+
+increment counter everytime user makes a guess. this applies when we call stats
+
+round.guesses
+card.guesses
+
+find_or_update/find_or_create this specific guess if the user guessed this card before we add to counter
+
+all the card.guesses with the counter of 1 is the first try correct
+or round.guesses with a counter of 1 is the first trye correct
+
+=end
--- db/migrate/20170922121437_create_guesses.rb
+++ db/migrate/20170922121437_create_guesses.rb
@@ -10,3 +10,4 @@ class CreateGuesses < ActiveRecord::Migration[5.0]
     end
   end
 end
+
