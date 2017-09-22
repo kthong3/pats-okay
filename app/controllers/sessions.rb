c06787782035dbeda5c02112dfe6ec6ddf61143f
Sam Tietze <samtietze@gmail.com>
2017-09-22 16:54:29 -0500

    Create login and logout routes, forms, and error handling partial

--- app/controllers/sessions.rb
+++ app/controllers/sessions.rb
@@ -3,7 +3,17 @@ get '/login' do
 end
 
 post '/login' do
-  @user = User.find_by(username: params[:username])
+  @user = User.authenticate(params[:username], params[:password])
+  if @user
+    session[:user_id] = @user.id
+    redirect '/decks'
+  else
+    @errors = ["Please enter a valid username please please also password please"]
+    erb :'sessions/new'
+  end
+end
 
-
+delete '/logout' do
+  session.delete(:user_id)
+  redirect '/decks'
 end
